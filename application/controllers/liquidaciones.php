b8565c499000e53636d9af4704fa39ff035d2bed
michael ortiz <michael.ortiz@turrisystem.com>
2015-02-23 15:51:51 -0500

    mejorada visualizacion de error en la modal de liquidacion, arreglada redireccion hacia la vista correspondiente si hay error de validacion en la fecha de pago

--- application/controllers/liquidaciones.php
+++ application/controllers/liquidaciones.php
@@ -32,6 +32,7 @@ class Liquidaciones extends MY_Controller {
 
           if ($this->ion_auth->is_admin() || $this->ion_auth->in_menu('liquidaciones/liquidar')){
 
+              $this->data['errorModal']=$this->session->flashdata('errorModal');
               $this->data['successmessage']=$this->session->flashdata('successmessage');
               $this->data['errormessage']=$this->session->flashdata('errormessage');
               $this->data['infomessage']=$this->session->flashdata('infomessage');
@@ -83,7 +84,8 @@ class Liquidaciones extends MY_Controller {
       if ($this->ion_auth->logged_in()){
 
           if ($this->ion_auth->is_admin() || $this->ion_auth->in_menu('liquidaciones/liquidar')){
-
+ 
+              $this->data['errorModal']=$this->session->flashdata('errorModal');
               $this->data['successmessage']=$this->session->flashdata('successmessage');
               $this->data['errormessage']=$this->session->flashdata('errormessage');
               $this->data['infomessage']=$this->session->flashdata('infomessage');
@@ -418,17 +420,38 @@ class Liquidaciones extends MY_Controller {
                  
                     if(isset($_POST['fecha_pago_'.$i]) && $_POST['fecha_pago_'.$i] == '')
                     {
-                        $this->session->set_flashdata('accion', 'liquidado');
+                        $this->session->set_flashdata('errorModal', '<strong>Error!</strong> Debe Ingresar una Fecha para el Pago.');
+                        $this->session->set_flashdata('accion', 'liquidado');                        
                         $this->session->set_flashdata('errormessage', '<strong>Error!</strong> Debe Ingresar una Fecha para el Pago.');
-                        redirect(base_url().'index.php/liquidaciones/liquidartramites/'.$id);
+                        
+                        //Valida para redirecionar a la vista respectiva
+                        //tramite o contrato
+                        if ($this->input->post('contratoid'))
+                        {
+                            redirect(base_url().'index.php/liquidaciones/liquidar/'.$id);
+                        }elseif ($this->input->post('tramiteid'))
+                            {
+                                redirect(base_url().'index.php/liquidaciones/liquidartramites/'.$id);
+                            }
+                        
 
                     }elseif(isset($_POST['fecha_pago_'.$i]) && $_POST['fecha_pago_'.$i] != '')
                         {
                             if(strtotime($_POST['fecha_pago_'.$i]) > strtotime(date('Y-m-d')))
                             {
+                                $this->session->set_flashdata('errorModal', '<strong>Error!</strong> la Fecha de Pago no Puede ser Mayor al Dia actual.');
                                 $this->session->set_flashdata('accion', 'liquidado');
                                 $this->session->set_flashdata('errormessage', '<strong>Error!</strong> la Fecha de Pago no Puede ser Mayor al Dia actual.');
-                                redirect(base_url().'index.php/liquidaciones/liquidartramites/'.$id);
+                                
+                                //Valida para redirecionar a la vista respectiva
+                                //tramite o contrato
+                                if ($this->input->post('contratoid'))
+                                {
+                                    redirect(base_url().'index.php/liquidaciones/liquidar/'.$id);
+                                }elseif ($this->input->post('tramiteid'))
+                                    {
+                                        redirect(base_url().'index.php/liquidaciones/liquidartramites/'.$id);
+                                    }
                             }
                         }
 
