2e863999920bfb4f9a2835bc63ade3b594603ee8
michael ortiz <michael.ortiz@turrisystem.com>
2015-04-13 17:22:01 -0500

    agregada configuracion para zona horaria

--- index.php
+++ index.php
@@ -43,6 +43,10 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+ /*
+ * Zona Horaria
+ */
+    date_default_timezone_set('America/Bogota');
 
 /*
  *---------------------------------------------------------------
