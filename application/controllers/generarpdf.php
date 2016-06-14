c08004a1668e41a1e7e313bcb8018ff67bc28d77
lramirez <leonardo.ramirez@turrisystem.com>
2016-06-14 17:50:32 -0500

    versionados cambios en produccion de archivo generar pdf

--- application/controllers/generarpdf.php
+++ application/controllers/generarpdf.php
@@ -12,55 +12,7 @@ require_once APPPATH.'/libraries/barcodegen/class/BCGFontFile.php';
 require_once APPPATH.'/libraries/barcodegen/class/BCGColor.php';
 require_once APPPATH.'/libraries/barcodegen/class/BCGDrawing.php';
 require_once APPPATH.'/libraries/barcodegen/class/BCGgs1128.barcode.php';
-// // Including all required classes
-// require_once('class/BCGFontFile.php');
-// require_once('class/BCGColor.php');
-// require_once('class/BCGDrawing.php');
 
-// // Including the barcode technology
-// //require_once('class/BCGcode39.barcode.php');
-// require_once('class/BCGgs1128.barcode.php');
-
-// // Loading Font
-// $font = new BCGFontFile('./font/Arial.ttf', 18);
-
-// // Don't forget to sanitize user inputs
-// $text = '(415)7709140080127(8020)7313100127(3900)172874000';
-
-// // The arguments are R, G, B for color.
-// $color_black = new BCGColor(0, 0, 0);
-// $color_white = new BCGColor(255, 255, 255);
-
-// $drawException = null;
-// try {
-//     $code = new BCGgs1128();
-//     $code->setScale(2); // Resolution
-//     $code->setThickness(30); // Thickness
-//     $code->setForegroundColor($color_black); // Color of bars
-//     $code->setBackgroundColor($color_white); // Color of spaces
-//     $code->setFont(0); // Font (or 0)
-//     $code->parse($text); // Text
-// } catch(Exception $exception) {
-//     $drawException = $exception;
-// }
-
-// /* Here is the list of the arguments
-// 1 - Filename (empty : display on screen)
-// 2 - Background color */
-// $drawing = new BCGDrawing('himan.png', $color_white);
-// if($drawException) {
-//     $drawing->drawException($drawException);
-// } else {
-//     $drawing->setBarcode($code);
-//     $drawing->draw();
-// }
-
-// // Header that says it is an image (remove it if you save the barcode to a file)
-// // header('Content-Type: image/png');
-// // header('Content-Disposition: inline; filename="barcode.png"');
-
-// // Draw (or save) the image into PNG format.
-// $drawing->finish(BCGDrawing::IMG_FORMAT_PNG);
 
 class Generarpdf extends CI_controller {
     
@@ -253,7 +205,7 @@ class Generarpdf extends CI_controller {
 
           if ($this->uri->segment(3)==''){
                redirect(base_url().'index.php/error_404');
-          }
+          } 
 
 
           //verifica que el usuario que llama el metodo
@@ -279,7 +231,17 @@ class Generarpdf extends CI_controller {
                            $this->data['estampilla'] = $this->liquidaciones_model->getfactura_legalizada($this->uri->segment(3),$doc=TRUE); 
                       }
 
-                  
+                    /*
+                    * Determina el string de contingencia
+                    */                  
+                    $objContin = $this->codegen_model->get('adm_parametros','para_contingencia','para_id = 1',1,NULL,true);
+                    if($objContin->para_contingencia == 1)
+                    {
+                        $this->data['contingencia'] = 'C';
+                    }else
+                        {
+                            $this->data['contingencia'] = '';
+                        }
               
                   $estampilla=$this->data['estampilla'];  
 
@@ -331,7 +293,7 @@ class Generarpdf extends CI_controller {
                   // set font
                    $pdf->SetFont('times', '', 8);
                    $pdf->AddPage('L',array(92,141));
-                   $this->data['params'] = TCPDF_STATIC::serializeTCPDFtagParameters(array('http://190.85.28.74:8086/estampillas-pro/index.php/qr/q/'.$this->uri->segment(3), 'QRCODE,H', 110, 57.5, 16, 16, $style, 'T'));
+                   $this->data['params'] = TCPDF_STATIC::serializeTCPDFtagParameters(array('http://www.aplicativosenlinea.net:8081/estampillas-pro/index.php/qr/q/'.$this->uri->segment(3), 'QRCODE,H', 110, 56, 16, 16, $style, 'T'));
                    $html = $this->load->view('generarpdf/generarpdf_estampillalegalizada', $this->data, TRUE);  
                 
                    $pdf->writeHTML($html, true, false, true, false, '');
