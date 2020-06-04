27e4f81ea6c83282cb4b1eb8fe65b36922a0d365
Monica Gutierrez <monica.gutierrez@turrisystem.com>
2020-06-04 08:56:30 -0500

    correccion barcode

---
+++ application/views/generarBarcode/barcodeFacturas.php
@@ -0,0 +1,11 @@
+<?php if( ! defined('BASEPATH') ) exit('No direct script access allowed'); ?>
+
+<!-- <tcpdf method="write1DBarcode" params="<?php echo $params; ?>" /> -->
+<div style="width: 100%">
+	<center>
+		
+	<img src="<?php echo $this->config->item('application_root'); ?>application/libraries/barcodegen/<?php echo $codebar ?>.png" width="300" height="40" align="center">
+	<br>
+		<small><?php echo $codebar; ?></small>
+	</center>
+</div>
